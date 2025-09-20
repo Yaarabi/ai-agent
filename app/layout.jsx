ce034653ba4499767ff0bd2ca5de2f18225488ed
Yaarabi <yaarabi318@gmail.com>
2025-09-20 01:11:08 +0100

    update favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,14 +14,12 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Yura Ai",
   description: "Yura Ai agent",
+  icons: "/favicon.png",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.png" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
